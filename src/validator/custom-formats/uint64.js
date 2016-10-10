eea7493d2b88b834542deca9ce2f9ef38842af0b
Chris Stoll <cstoll@ciena.com>
2016-10-10 09:23:51 -0700

    change uint64 implementation, updated tests

--- src/validator/custom-formats/uint64.js
+++ src/validator/custom-formats/uint64.js
@@ -1,6 +1,4 @@
-import rangeFnFactory from './range-fn-factory'
-
-const uInt64Pattern = /^(\d{1,19}|1[1-7]\d{18}|18[1-3]\d{17}|184[1-3]\d{16}|1844[1-5]\d{15}|18446[1-6]\d{14}|184467[1-3]\d{13}|1844674[1-3]\d{12}|184467440[1-6]\d{10}|1844674407[1-2]\d{9}|18446744073[1-6]\d{8}|1844674407370[1-8]\d{6}|18446744073709[1-4]\d{5}|184467440737095[1-4]\d{4}|18446744073709551[1-5]\d{2}|1844674407370955160[1-9]|1844674407370955161[1-5])$/
+const MAX_VALUE = '18446744073709551615'
 
 /**
  * Validate value as an unsigned 64-bit integer
@@ -8,5 +6,28 @@ const uInt64Pattern = /^(\d{1,19}|1[1-7]\d{18}|18[1-3]\d{17}|184[1-3]\d{16}|1844
  * @returns {Boolean} whether or not value is valid
  */
 export default function (value) {
-  return uInt64Pattern.test(`${value}`)  
+  value = `${value}`
+
+ // Ensure value is a natural number (positive integers)
+  if (!/^\d+$/.test(value)) {
+    return false
+  }
+
+ // If value has less digits than max value then we can safely say it is valid
+  if (value.length < MAX_VALUE.length) {
+    return true
+  }
+
+ // If value has more digits than max value then we can safely say it is not valid
+  if (value.length > MAX_VALUE.length) {
+    return false
+  }
+
+  const firstHalf = parseInt(value.substr(0, 10))
+  const secondHalf = parseInt(value.substr(10))
+
+  return !(
+    firstHalf > 1844674407 ||
+    (firstHalf === 1844674407 && secondHalf > 3709551615)
+  )
 }
