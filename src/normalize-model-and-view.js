57d1c63b751814c1c4691eef291eb6cefb5a617c
Matthew Dahl <mdahl@ciena.com>
2017-05-01 12:04:48 -0700

    Work out some of the kinks

--- src/normalize-model-and-view.js
+++ src/normalize-model-and-view.js
@@ -41,10 +41,16 @@ export function normalizeCell (state, cell, parents) {
     const isInternal = cell.internal === true
     const id = isInternal ? `internal.${cell.id}` : cell.id
     const modelPath = getModelPath(id)
+    const viewState = normalizeCellProperties(parents.concat(cell), id)
+
+    // This makes it so parents are the shallow clones created within
+    // normalizeCellProperties() which are now in state.view
+    parents = viewState.parents.slice(0, parents.length)
 
     state = {
       model: addBunsenModelProperty(state.model, cell.model, modelPath),
-      view: normalizeCellProperties(parents.concat(cell), id)
+      parents,
+      view: viewState.view
     }
   }
 
@@ -54,6 +60,7 @@ export function normalizeCell (state, cell, parents) {
 export function normalizeCellProperties (nodes, modelPath) {
   const view = Object.assign({}, nodes.shift())
   const next = nodes.shift()
+  const parents = [view]
 
   let pointer = view
 
@@ -67,6 +74,8 @@ export function normalizeCellProperties (nodes, modelPath) {
     view.cells = view.cells.slice(0, index) // get all cells before cell
       .concat(pointer) // shallow clone cell
       .concat(view.cells.slice(index + 1)) // get all cells after cell
+
+    parents.push(view.cells, pointer)
   } else if (next === view.cellDefinitions) {
     view.cellDefinitions = Object.assign({}, view.cellDefinitions)
     const def = nodes.shift()
@@ -80,6 +89,7 @@ export function normalizeCellProperties (nodes, modelPath) {
     })
 
     pointer = view.cellDefinitions[defKey] = Object.assign({}, def)
+    parents.push(view.cellDefinitions, pointer)
   }
 
   while (nodes.length) {
@@ -95,6 +105,7 @@ export function normalizeCellProperties (nodes, modelPath) {
         .concat(clone) // shallow clone parent
         .concat(pointer.children.slice(index + 1)) // get all items after parent
 
+      parents.push(pointer.children, clone)
       pointer = clone
     }
   }
@@ -103,16 +114,20 @@ export function normalizeCellProperties (nodes, modelPath) {
   delete pointer.id
   delete pointer.internal
 
-  return view
+  return {
+    parents,
+    view
+  }
 }
 
 export function normalizeCells (state) {
-  if (!state.view || !state.view.cells) return state
-
-  const parents = [state.view, state.view.cells]
+  if (!state.view || !Array.isArray(state.view.cells)) return state
 
   return state.view.cells.reduce(
-    (_state, cell) => normalizeCell(_state, cell, parents),
+    (_state, cell) => {
+      const parents = [_state.view, _state.view.cells]
+      return normalizeCell(_state, cell, parents)
+    },
     state
   )
 }
@@ -120,14 +135,25 @@ export function normalizeCells (state) {
 export function normalizeChildren (state, cell, parents) {
   if (!Array.isArray(cell.children)) return state
 
-  return cell.children.reduce(
+  const newState = cell.children.reduce(
     (_state, child) => {
-      parents = Array.from(parents)
-      parents.push(cell, cell.children)
-      return normalizeCell(_state, parents)
+      let childParents
+
+      if (_state.parents) {
+        childParents = _state.parents.slice(0, parents.length + 2)
+      } else {
+        childParents = parents.concat([cell, cell.children])
+      }
+
+      return normalizeCell(_state, child, childParents)
     },
     state
   )
+
+  // Make sure outbound parents are just clones of inbound parents
+  newState.parents = newState.parents.slice(0, parents.length)
+
+  return newState
 }
 
 export default function ({model, view}) {
--- tests/normalize-model-and-view-test.js
+++ tests/normalize-model-and-view-test.js
@@ -147,6 +147,10 @@ describe('normalize model and view', function () {
           },
           type: 'object'
         },
+        parents: [
+          actual.view,
+          actual.view.cells
+        ],
         view: {
           cells: [
             {
@@ -205,6 +209,10 @@ describe('normalize model and view', function () {
           },
           type: 'object'
         },
+        parents: [
+          actual.view,
+          actual.view.cells
+        ],
         view: {
           cells: [
             {
@@ -281,6 +289,10 @@ describe('normalize model and view', function () {
           },
           type: 'object'
         },
+        parents: [
+          actual.view,
+          actual.view.cells
+        ],
         view: {
           cells: [
             {
@@ -371,6 +383,10 @@ describe('normalize model and view', function () {
           },
           type: 'object'
         },
+        parents: [
+          actual.view,
+          actual.view.cells
+        ],
         view: {
           cells: [
             {
@@ -383,6 +399,275 @@ describe('normalize model and view', function () {
                 }
               ],
               model: 'internal.test'
+            }
+          ],
+          type: 'form',
+          version: '2.0'
+        }
+      })
+    })
+
+    it('normalizes internal cell with nested children', () => {
+      const state = Object.freeze({
+        model: Object.freeze({
+          properties: Object.freeze({
+            foo: Object.freeze({
+              type: 'string'
+            })
+          }),
+          type: 'object'
+        }),
+        view: Object.freeze({
+          cells: Object.freeze([
+            Object.freeze({
+              children: Object.freeze([
+                Object.freeze({
+                  children: Object.freeze([
+                    Object.freeze({
+                      model: 'bar'
+                    }),
+                    Object.freeze({
+                      model: 'baz'
+                    })
+                  ]),
+                  model: 'foo'
+                })
+              ]),
+              id: 'test',
+              internal: true,
+              model: Object.freeze({
+                properties: Object.freeze({
+                  foo: Object.freeze({
+                    properties: Object.freeze({
+                      bar: Object.freeze({
+                        type: 'number'
+                      }),
+                      baz: Object.freeze({
+                        type: 'string'
+                      })
+                    }),
+                    type: 'object'
+                  })
+                }),
+                type: 'object'
+              })
+            })
+          ]),
+          type: 'form',
+          version: '2.0'
+        })
+      })
+
+      const cell = state.view.cells[0]
+      const parents = [state.view, state.view.cells]
+      const actual = stuff.normalizeCell(state, cell, parents)
+
+      expect(actual).to.eql({
+        model: {
+          properties: {
+            foo: {
+              type: 'string'
+            },
+            internal: {
+              properties: {
+                test: {
+                  properties: {
+                    foo: {
+                      properties: {
+                        bar: {
+                          type: 'number'
+                        },
+                        baz: {
+                          type: 'string'
+                        }
+                      },
+                      type: 'object'
+                    }
+                  },
+                  type: 'object'
+                }
+              },
+              type: 'object'
+            }
+          },
+          type: 'object'
+        },
+        parents: [
+          actual.view,
+          actual.view.cells
+        ],
+        view: {
+          cells: [
+            {
+              children: [
+                {
+                  children: [
+                    {
+                      model: 'bar'
+                    },
+                    {
+                      model: 'baz'
+                    }
+                  ],
+                  model: 'foo'
+                }
+              ],
+              model: 'internal.test'
+            }
+          ],
+          type: 'form',
+          version: '2.0'
+        }
+      })
+    })
+
+    it('normalizes nested cell without children', () => {
+      const state = Object.freeze({
+        model: Object.freeze({
+          properties: {},
+          type: 'object'
+        }),
+        view: Object.freeze({
+          cells: Object.freeze([
+            Object.freeze({
+              children: Object.freeze([
+                Object.freeze({
+                  children: Object.freeze([
+                    Object.freeze({
+                      id: 'foo',
+                      model: Object.freeze({
+                        type: 'string'
+                      })
+                    })
+                  ])
+                })
+              ])
+            })
+          ]),
+          type: 'form',
+          version: '2.0'
+        })
+      })
+
+      const cell = state.view.cells[0]
+      const parents = [state.view, state.view.cells]
+      const actual = stuff.normalizeCell(state, cell, parents)
+
+      expect(actual).to.eql({
+        model: {
+          properties: {
+            foo: {
+              type: 'string'
+            }
+          },
+          type: 'object'
+        },
+        parents: [
+          actual.view,
+          actual.view.cells
+        ],
+        view: {
+          cells: [
+            {
+              children: [
+                {
+                  children: [
+                    {
+                      model: 'foo'
+                    }
+                  ]
+                }
+              ]
+            }
+          ],
+          type: 'form',
+          version: '2.0'
+        }
+      })
+    })
+
+    it('normalizes nested sibling cells without children', () => {
+      const state = Object.freeze({
+        model: Object.freeze({
+          properties: {
+            test: Object.freeze({
+              type: 'string'
+            })
+          },
+          type: 'object'
+        }),
+        view: Object.freeze({
+          cells: Object.freeze([
+            Object.freeze({
+              children: Object.freeze([
+                Object.freeze({
+                  model: 'test'
+                }),
+                Object.freeze({
+                  children: Object.freeze([
+                    Object.freeze({
+                      id: 'foo',
+                      model: Object.freeze({
+                        type: 'string'
+                      })
+                    }),
+                    Object.freeze({
+                      id: 'bar',
+                      model: Object.freeze({
+                        type: 'string'
+                      })
+                    })
+                  ])
+                })
+              ])
+            })
+          ]),
+          type: 'form',
+          version: '2.0'
+        })
+      })
+
+      const cell = state.view.cells[0]
+      const parents = [state.view, state.view.cells]
+      const actual = stuff.normalizeCell(state, cell, parents)
+
+      expect(actual).to.eql({
+        model: {
+          properties: {
+            bar: {
+              type: 'string'
+            },
+            foo: {
+              type: 'string'
+            },
+            test: {
+              type: 'string'
+            }
+          },
+          type: 'object'
+        },
+        parents: [
+          actual.view,
+          actual.view.cells
+        ],
+        view: {
+          cells: [
+            {
+              children: [
+                {
+                  model: 'test'
+                },
+                {
+                  children: [
+                    {
+                      model: 'foo'
+                    },
+                    {
+                      model: 'bar'
+                    }
+                  ]
+                }
+              ]
             }
           ],
           type: 'form',
@@ -410,7 +695,13 @@ describe('normalize model and view', function () {
       const nodes = [view, view.cells, view.cells[0]]
       const actual = stuff.normalizeCellProperties(nodes, 'test')
 
-      expect(actual).to.eql({
+      expect(actual.parents).to.eql([
+        actual.view,
+        actual.view.cells,
+        actual.view.cells[0]
+      ])
+
+      expect(actual.view).to.eql({
         cells: [
           {
             model: 'test'
@@ -449,7 +740,15 @@ describe('normalize model and view', function () {
 
       const actual = stuff.normalizeCellProperties(nodes, 'test')
 
-      expect(actual).to.eql({
+      expect(actual.parents).to.eql([
+        actual.view,
+        actual.view.cells,
+        actual.view.cells[0],
+        actual.view.cells[0].children,
+        actual.view.cells[0].children[0]
+      ])
+
+      expect(actual.view).to.eql({
         cells: [
           {
             children: [
@@ -486,7 +785,13 @@ describe('normalize model and view', function () {
       const nodes = [view, view.cellDefinitions, view.cellDefinitions.main]
       const actual = stuff.normalizeCellProperties(nodes, 'test')
 
-      expect(actual).to.eql({
+      expect(actual.parents).to.eql([
+        actual.view,
+        actual.view.cellDefinitions,
+        actual.view.cellDefinitions.main
+      ])
+
+      expect(actual.view).to.eql({
         cellDefinitions: {
           main: {
             model: 'test'
@@ -535,7 +840,15 @@ describe('normalize model and view', function () {
 
       const actual = stuff.normalizeCellProperties(nodes, 'test')
 
-      expect(actual).to.eql({
+      expect(actual.parents).to.eql([
+        actual.view,
+        actual.view.cellDefinitions,
+        actual.view.cellDefinitions.main,
+        actual.view.cellDefinitions.main.children,
+        actual.view.cellDefinitions.main.children[0]
+      ])
+
+      expect(actual.view).to.eql({
         cellDefinitions: {
           main: {
             children: [
@@ -553,6 +866,186 @@ describe('normalize model and view', function () {
         type: 'form',
         version: '2.0'
       })
+    })
+  })
+
+  it('normalizeCells() normalizes various types of cells', () => {
+    const state = Object.freeze({
+      model: Object.freeze({
+        properties: Object.freeze({
+          foo: Object.freeze({
+            type: 'string'
+          })
+        }),
+        type: 'object'
+      }),
+      view: Object.freeze({
+        cells: Object.freeze([
+          Object.freeze({
+            model: 'foo'
+          }),
+          Object.freeze({
+            id: 'alpha',
+            model: Object.freeze({
+              type: 'boolean'
+            })
+          }),
+          Object.freeze({
+            id: 'bravo',
+            internal: true,
+            model: Object.freeze({
+              type: 'number'
+            })
+          }),
+          Object.freeze({
+            children: Object.freeze([
+              Object.freeze({
+                id: 'charlie',
+                model: Object.freeze({
+                  type: 'string'
+                })
+              }),
+              Object.freeze({
+                id: 'delta',
+                internal: true,
+                model: {
+                  type: 'number'
+                }
+              })
+            ]),
+            label: 'Test'
+          })
+        ]),
+        type: 'form',
+        version: '2.0'
+      })
+    })
+
+    const actual = stuff.normalizeCells(state)
+
+    expect(actual).to.eql({
+      model: {
+        properties: {
+          alpha: {
+            type: 'boolean'
+          },
+          charlie: {
+            type: 'string'
+          },
+          foo: {
+            type: 'string'
+          },
+          internal: {
+            properties: {
+              bravo: {
+                type: 'number'
+              },
+              delta: {
+                type: 'number'
+              }
+            },
+            type: 'object'
+          }
+        },
+        type: 'object'
+      },
+      parents: [
+        actual.view,
+        actual.view.cells
+      ],
+      view: {
+        cells: [
+          {
+            model: 'foo'
+          },
+          {
+            model: 'alpha'
+          },
+          {
+            model: 'internal.bravo'
+          },
+          {
+            children: [
+              {
+                model: 'charlie'
+              },
+              {
+                model: 'internal.delta'
+              }
+            ],
+            label: 'Test'
+          }
+        ],
+        type: 'form',
+        version: '2.0'
+      }
+    })
+  })
+
+  it('normalizeChildren() normalizes children', () => {
+    const state = Object.freeze({
+      model: Object.freeze({
+        properties: Object.freeze({
+          foo: Object.freeze({
+            type: 'string'
+          })
+        }),
+        type: 'object'
+      }),
+      view: Object.freeze({
+        cells: Object.freeze([
+          Object.freeze({
+            children: Object.freeze([
+              Object.freeze({
+                id: 'bar',
+                model: Object.freeze({
+                  type: 'number'
+                })
+              })
+            ]),
+            label: 'Test'
+          })
+        ]),
+        type: 'form',
+        version: '2.0'
+      })
+    })
+
+    const cell = state.view.cells[0]
+    const parents = [state.view, state.view.cells]
+
+    const actual = stuff.normalizeChildren(state, cell, parents)
+
+    expect(actual).to.eql({
+      model: {
+        properties: {
+          bar: {
+            type: 'number'
+          },
+          foo: {
+            type: 'string'
+          }
+        },
+        type: 'object'
+      },
+      parents: [
+        actual.view,
+        actual.view.cells
+      ],
+      view: {
+        cells: [
+          {
+            children: [
+              {
+                model: 'bar'
+              }
+            ],
+            label: 'Test'
+          }
+        ],
+        type: 'form',
+        version: '2.0'
+      }
     })
   })
 })
