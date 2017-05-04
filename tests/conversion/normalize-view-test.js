3530cbd512e5dda1833c112379e62dc08e9ae4e5
Matthew Dahl <mdahl@ciena.com>
2017-05-04 08:07:49 -0700

    Make normalizeView() method pure, as in don't mutate input

--- tests/conversion/normalize-view-test.js
+++ tests/conversion/normalize-view-test.js
@@ -3,44 +3,44 @@ var normalizeView = require('../../lib/conversion/normalize-view').default
 
 describe('normalizeView()', function () {
   it(' normalizes view as expected', function () {
-    const input = {
-      cellDefinitions: {
-        test: {
-          children: [
-            {model: 'foo[0]'},
-            {model: 'foo[0].bar'},
-            {model: 'alpha[0][1]'},
-            {model: 'alpha[0][1].bravo'}
-          ]
-        },
-        model: {
-          children: [
-            {model: 'foo[0]'},
-            {model: 'foo[0].bar'},
-            {model: 'alpha[0][1]'},
-            {model: 'alpha[0][1].bravo'}
-          ]
-        }
-      },
-      cells: [
-        {model: 'foo[0]'},
-        {model: 'foo[0].bar'},
-        {model: 'alpha[0][1]'},
-        {model: 'alpha[0][1].bravo'},
-        {
-          children: [
-            {model: 'foo[0]'},
-            {model: 'foo[0].bar'},
-            {model: 'alpha[0][1]'},
-            {model: 'alpha[0][1].bravo'}
-          ]
-        },
-        {extends: 'test'},
-        {extends: 'model'}
-      ],
+    const input = Object.freeze({
+      cellDefinitions: Object.freeze({
+        test: Object.freeze({
+          children: Object.freeze([
+            Object.freeze({model: 'foo[0]'}),
+            Object.freeze({model: 'foo[0].bar'}),
+            Object.freeze({model: 'alpha[0][1]'}),
+            Object.freeze({model: 'alpha[0][1].bravo'})
+          ])
+        }),
+        model: Object.freeze({
+          children: Object.freeze([
+            Object.freeze({model: 'foo[0]'}),
+            Object.freeze({model: 'foo[0].bar'}),
+            Object.freeze({model: 'alpha[0][1]'}),
+            Object.freeze({model: 'alpha[0][1].bravo'})
+          ])
+        })
+      }),
+      cells: Object.freeze([
+        Object.freeze({model: 'foo[0]'}),
+        Object.freeze({model: 'foo[0].bar'}),
+        Object.freeze({model: 'alpha[0][1]'}),
+        Object.freeze({model: 'alpha[0][1].bravo'}),
+        Object.freeze({
+          children: Object.freeze([
+            Object.freeze({model: 'foo[0]'}),
+            Object.freeze({model: 'foo[0].bar'}),
+            Object.freeze({model: 'alpha[0][1]'}),
+            Object.freeze({model: 'alpha[0][1].bravo'})
+          ])
+        }),
+        Object.freeze({extends: 'test'}),
+        Object.freeze({extends: 'model'})
+      ]),
       type: 'form',
       version: '2.0'
-    }
+    })
 
     expect(normalizeView(input)).to.eql({
       cellDefinitions: {
